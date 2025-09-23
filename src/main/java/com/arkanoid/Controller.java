609bce4a5deef8407a27755a695c7336a3862bff
HoanhTQ <24020142@vnu.edu.vn>
2025-09-23 22:15:56 +0700

    Change Circle to Entity & add two PowerUps
    (cherry picked from commit 72c48926b19915bb366997c45a5e651b5bbffd52)

--- src/main/java/com/arkanoid/Controller.java
+++ src/main/java/com/arkanoid/Controller.java
@@ -1,7 +1,9 @@
 package com.arkanoid;
 
 import java.net.URL;
+import java.util.HashSet;
 import java.util.ResourceBundle;
+import java.util.Set;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
@@ -11,8 +13,8 @@ import javafx.fxml.Initializable;
 import javafx.geometry.Bounds;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
@@ -20,10 +22,8 @@ public class Controller implements Initializable {
     @FXML
     private AnchorPane scene;
 
-    @FXML
-    private Circle circle;
+    private Ball ball;
 
-    @FXML
     private Paddle paddle;
 
     @FXML
@@ -35,15 +35,18 @@ public class Controller implements Initializable {
     public static double deltaX = 2;
     public static double deltaY = 2;
 
+    public static final Set<KeyCode> pressedKeys = new HashSet<>();
+
     Timeline timeline = new Timeline(new KeyFrame(Duration.millis(10), new EventHandler<ActionEvent>() {
     long LastTime = System.nanoTime();
         @Override
         public void handle(ActionEvent actionEvent) {
             paddle.update(scene);
-            paddle.checkCollisionPaddle(circle);
 
-            circle.setLayoutX(circle.getLayoutX() + deltaX);
-            circle.setLayoutY(circle.getLayoutY() + deltaY);
+            paddle.checkCollisionPaddle(ball.getCircle());
+
+            ball.getShape().setLayoutX(ball.getShape().getLayoutX() + deltaX);
+            ball.getShape().setLayoutY(ball.getShape().getLayoutY() + deltaY);
 
             long CurrentTime = System.nanoTime();
             double DeltaTime = (CurrentTime - LastTime) / 1_000_000_000.0;
@@ -52,14 +55,14 @@ public class Controller implements Initializable {
             EnemiesManager.updateEnemies(DeltaTime);
 
             if (BrickManager.brick_remain > 0) {
-                BrickManager.update(circle, scene);
+                BrickManager.update(ball, scene);
             } else {
-                startButton.setVisible(true);
-                timeline.stop();
+                gameOver();
             }
 
             PowerUpManager.movePowerUps(scene);
             PowerUpManager.checkCollisionPowerUps(paddle, scene);
+            PowerUpManager.update(paddle, scene);
 
             checkCollisionScene(scene);
             checkCollisionBottomZone();
@@ -75,45 +78,72 @@ public class Controller implements Initializable {
     void startGameButtonAction(ActionEvent event) {
         startButton.setVisible(false);
 
-        circle.setLayoutX(10);
-        circle.setLayoutY(10);
+        ball = new Ball(10, 10, 5);
+        ball.getCircle().setLayoutX(10);
+        ball.getCircle().setLayoutY(10);
+        scene.getChildren().add(ball.getShape());
 
         BrickManager.createBricks(scene);
         EnemiesManager.CreateEnemies(scene);
 
         paddle = new Paddle(244, 360, 80, 10);
-        scene.getChildren().add(paddle.getPos());
+        scene.getChildren().add(paddle.getShape());
+
+        scene.setOnKeyPressed(e -> pressedKeys.add(e.getCode()));
+        scene.setOnKeyReleased(e -> pressedKeys.remove(e.getCode()));
+        scene.requestFocus();
 
         timeline.play();
     }
 
     public void checkCollisionScene(Node node) {
         Bounds bounds = node.getBoundsInLocal();
-        boolean rightBorder = circle.getLayoutX() >= (bounds.getMaxX() - circle.getRadius());
-        boolean leftBorder = circle.getLayoutX() <= (bounds.getMinX() + circle.getRadius());
-        boolean bottomBorder = circle.getLayoutY() >= (bounds.getMaxY() - circle.getRadius());
-        boolean topBorder = circle.getLayoutY() <= (bounds.getMinY() + circle.getRadius());
+        boolean rightBorder = ball.getCircle().getLayoutX() >= (bounds.getMaxX() - 3 * ball.getCircle().getRadius());
+        boolean leftBorder = ball.getCircle().getLayoutX() <= (bounds.getMinX() + ball.getCircle().getRadius());
+        boolean bottomBorder = ball.getCircle().getLayoutY() >= (bounds.getMaxY() - ball.getCircle().getRadius());
+        boolean topBorder = ball.getCircle().getLayoutY() <= (bounds.getMinY() + ball.getCircle().getRadius());
+
+//        System.out.println(ball.getCircle().getLayoutX() + " radius: " + ball.getCircle().getRadius() + " rightBorder: " + bounds.getMaxX());
 
         if (rightBorder || leftBorder) {
             deltaX *= -1;
+            if (PowerUpManager.powerUpState[0] > 0) {
+                PowerUpManager.powerUpState[0]--;
+            }
         }
 
         if (bottomBorder || topBorder) {
             deltaY *= -1;
+            if (PowerUpManager.powerUpState[0] > 0) {
+                PowerUpManager.powerUpState[0]--;
+            }
         }
 
     }
 
     public void checkCollisionBottomZone() {
-        if (circle.getBoundsInParent().intersects(bottomZone.getBoundsInParent())) {
-            timeline.stop();
-            for (Brick brick : BrickManager.bricks) {
-                scene.getChildren().remove(brick.getPos());
-            }
-            BrickManager.bricks.clear();
-            scene.getChildren().remove(paddle.getPos());
-            startButton.setVisible(true);
+        if (ball.getCircle().getBoundsInParent().intersects(bottomZone.getBoundsInParent())) {
+            gameOver();
         }
+    }
+
+    public void gameOver() {
+        timeline.stop();
+        for (Brick brick : BrickManager.bricks) {
+            scene.getChildren().remove(brick.getShape());
+        }
+        for (PowerUp powerUp : PowerUpManager.powerUps ){
+            scene.getChildren().remove(powerUp.getShape());
+        }
+        for (Entity projectile : PowerUpManager.projectiles ){
+            scene.getChildren().remove(projectile.getShape());
+        }
+        BrickManager.bricks.clear();
+        PowerUpManager.powerUps.clear();
+        PowerUpManager.projectiles.clear();
+        scene.getChildren().remove(paddle.getShape());
+        scene.getChildren().remove(ball.getShape());
+        startButton.setVisible(true);
     }
 
 }
