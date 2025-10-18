7443eaa1c388dcc45f0c788a6be97d1c67b942c4
JokerFav <tranvietbao2006@gmail.com>
2025-10-18 18:16:18 +0700

    Fix bugs in TriangleC

---
+++ src/main/java/com/arkanoid/Enemies/TriangleC.java
@@ -0,0 +1,96 @@
+package com.arkanoid.enemies;
+
+import com.arkanoid.brick.Brick;
+import com.arkanoid.brick.BrickManager;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+
+
+public class TriangleC extends Enemies {
+
+  private int moveCooldown;
+  double amplitude = 25;
+  double swingSpeed = 1.5;
+  double fallSpeed = 10;
+  double baseX = 0;
+  private final Image[][] images = new Image[2][12];
+  private int state = 0;
+  private int imageState;
+  private int imageCooldown;
+
+  public TriangleC(double x, double y, double w, double h) {
+    super(x, y, w, h);
+    baseX = x;
+    for (int i = 0; i < 2; i++) {
+      for (int j = 0; j < (i == 1 ? 12 : 8); j++) {
+        images[i][j] = new Image(getClass().getResource(
+            "/com/arkanoid/enemy/TriangleC/TriangleC" + i + j + ".png").toExternalForm());
+      }
+    }
+    state = 0;
+    imageState = 0;
+    imageCooldown = 20;
+  }
+
+  @Override
+  public boolean update(double deltaTime, AnchorPane scene) {
+    switch (state) {
+      case 0:
+
+        totalTime = totalTime + deltaTime;
+
+        double oldY = this.getRectangle().getY();
+        this.getRectangle().setY(this.getRectangle().getY() + fallSpeed * deltaTime);
+        if (this.checkCollisionEnemy()
+                || this.checkCollisionScene()
+                || this.checkCollisionBrick()) {
+            this.getRectangle().setY(oldY);
+        }
+
+        double changeX = amplitude * Math.sin(swingSpeed * totalTime);
+        double oldX = this.getRectangle().getX();
+        this.getRectangle().setX(baseX + changeX);
+        if (this.checkCollisionEnemy()
+                || this.checkCollisionScene()
+                || this.checkCollisionBrick()) {
+          this.getRectangle().setX(oldX);
+          totalTime = totalTime - deltaTime;
+        }
+
+        if (this.checkCollisionBall()) {
+          state = 1;
+          imageCooldown = 20;
+          imageState = 0;
+          moveCooldown = imageCooldown * 12;
+        } else {
+          imageCooldown--;
+          if (imageCooldown == 0) {
+            imageState++;
+            imageState %= 8;
+            imageCooldown = 20;
+          }
+        }
+        break;
+      case 1:
+        imageCooldown--;
+        moveCooldown--;
+        if (moveCooldown == 0) {
+          return true;
+        }
+        if (imageCooldown == 0) {
+          imageState++;
+          imageCooldown = 20;
+        }
+        break;
+      default:
+        break;
+    }
+
+    imageView.setImage(images[state][imageState]);
+    imageView.setX(this.getRectangle().getX() + this.getRectangle().getWidth() / 2
+        - images[state][imageState].getWidth() / 2);
+    imageView.setY(this.getRectangle().getY() + this.getRectangle().getHeight() / 2
+        - images[state][imageState].getHeight() / 2);
+    return false;
+  }
+}
--- src/main/java/com/arkanoid/score/Hp.java
+++ src/main/java/com/arkanoid/score/Hp.java
@@ -6,67 +6,69 @@ import javafx.scene.layout.AnchorPane;
 
 public class Hp {
 
-    private static int hp;
-    private ImageView paddleIcon;
-    private Digit[] showHp = new Digit[2];
+  private static int hp;
+  private ImageView paddleIcon;
+  private Digit[] showHp = new Digit[2];
 
-    public Hp(AnchorPane scene) {
-        Image lifeImage = new Image(getClass().getResource("/com/arkanoid/Paddle/paddle00.png").toExternalForm());
+  public Hp(AnchorPane scene) {
+    Image lifeImage = new Image(
+        getClass().getResource("/com/arkanoid/paddle/paddle00.png").toExternalForm());
 
-        double startX = 200;
-        double yPos = 74;
-        double iconWidth = 16;
-        double spacing = 8;
-        double digitWidth = 10;
-        double iconX = startX;
-        double digitTensX = iconX + iconWidth + spacing;
-        double digitUnitsX = digitTensX + spacing;
+    double startX = 200;
+    final double ypos = 74;
+    double iconWidth = 16;
+    double spacing = 8;
+    final double digitWidth = 10;
+    double iconX = startX;
+    double digitTensX = iconX + iconWidth + spacing;
+    final double digitUnitsX = digitTensX + spacing;
 
+    paddleIcon = new ImageView(lifeImage);
+    paddleIcon.setFitWidth(iconWidth);
+    paddleIcon.setPreserveRatio(true);
+    paddleIcon.setX(iconX);
+    paddleIcon.setY(ypos);
 
-        paddleIcon = new ImageView(lifeImage);
-        paddleIcon.setFitWidth(iconWidth);
-        paddleIcon.setPreserveRatio(true);
-        paddleIcon.setX(iconX);
-        paddleIcon.setY(yPos);
+    int displayHp = (hp > 0) ? hp - 1 : 0;
+    showHp[0] = new Digit((int) digitTensX, (int) ypos, (int) digitWidth, 16,
+        displayHp / 10); // Hàng chục
+    showHp[1] = new Digit((int) digitUnitsX, (int) ypos, (int) digitWidth, 16,
+        displayHp % 10);  // Hàng đơn vị
 
-        int displayHp = (hp > 0) ? hp - 1 : 0;
-        showHp[0] = new Digit((int)digitTensX, (int)yPos, (int)digitWidth, 16, displayHp / 10); // Hàng chục
-        showHp[1] = new Digit((int)digitUnitsX, (int)yPos, (int)digitWidth, 16, displayHp % 10);  // Hàng đơn vị
+    scene.getChildren().addAll(paddleIcon, showHp[0].getImageView(), showHp[1].getImageView());
+  }
 
-        scene.getChildren().addAll(paddleIcon, showHp[0].getImageView(), showHp[1].getImageView());
+  public static void resetHp() {
+    hp = 3;
+  }
+
+  public static void loseLife() {
+    if (hp > 0) {
+      hp--;
     }
+  }
 
-    public static void resetHp() {
-        hp = 3;
-    }
+  public static int getHp() {
+    return hp;
+  }
 
-    public static void loseLife() {
-        if (hp > 0) {
-            hp--;
-        }
-    }
+  public void updateDisplay() {
+    int displayHp = (hp > 0) ? hp - 1 : 0;
+    int tens = displayHp / 10;
+    int units = displayHp % 10;
 
-    public static int getHp() {
-        return hp;
-    }
+    showHp[0].change(tens);
+    showHp[1].change(units);
 
-    public void updateDisplay() {
-        int displayHp = (hp > 0) ? hp - 1 : 0;
-        int tens = displayHp / 10;
-        int units = displayHp % 10;
+    boolean isVisible = hp > 0;
+    paddleIcon.setVisible(isVisible);
+    showHp[0].getImageView().setVisible(isVisible);
+    showHp[1].getImageView().setVisible(isVisible);
+  }
 
-        showHp[0].change(tens);
-        showHp[1].change(units);
-
-        boolean isVisible = hp > 0;
-        paddleIcon.setVisible(isVisible);
-        showHp[0].getImageView().setVisible(isVisible);
-        showHp[1].getImageView().setVisible(isVisible);
-    }
-
-    public void clear(AnchorPane scene) {
-        scene.getChildren().remove(paddleIcon);
-        scene.getChildren().remove(showHp[0].getImageView());
-        scene.getChildren().remove(showHp[1].getImageView());
-    }
+  public void clear(AnchorPane scene) {
+    scene.getChildren().remove(paddleIcon);
+    scene.getChildren().remove(showHp[0].getImageView());
+    scene.getChildren().remove(showHp[1].getImageView());
+  }
 }
